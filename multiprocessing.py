a6d5bb872885548dfee33a1bf2b85a794336a077
linuxipho <remidechazelles@gmail.com>
2015-06-17 00:13:18 +0200

    A "class" version

--- multiprocessing.py
+++ multiprocessing.py
@@ -1,212 +1,95 @@
-__author__ = "linuxipho"
-
 #!/usr/bin/python3
 
 import os
 import sys
 import zipfile
-#from multiprocessing import Process
+# from multiprocessing import Process
 
 from PIL import Image
+
 Image.MAX_IMAGE_PIXELS = None
 
+
 class Builder:
-    """The main class"""
+    """
+        The main class
+    """
 
-    def __init__(self, bbox, mapname):
-        """Initialize the area variables"""
-
+    def __init__(self, bbox, title):
+        """
+            Initialize the area variables
+        """
+        self.title = title
         self.xmin = bbox[0]
         self.ymin = bbox[1]
         self.xmax = bbox[2]
         self.ymax = bbox[3]
-        self.path = os.path.dirname(os.path.realpath(__file__))
+        os.mkdir('temp')
+        os.mkdir('temp/{0}'.format(self.title))
+        os.mkdir('temp/{0}/tiles'.format(self.title))
 
+    def _tile(self, _src, _l, _i, _j, _iend, _jend, _aa, _bb):
+        """
+            Internal methode which create the requested tile
+        """
+        _extend = (_i, _j, _i + _iend, _j + _jend)
+        _t = _src.crop(_extend)
+        _t.save('temp/{}/tiles/{}x{}x{}.png'.format(self.title, _l, _aa, _bb), options='optimize')
 
-    def _tile(self,level,):
-        """Internal methode which create the requested tile"""
+    def run(self, _source, _level, _scale, _sample):
+        """
+            The main algorythme
+        """
 
-        extend = (j, i, j + xend, i + yend)
-        tile = src.crop(extend)
-        tile.save('temp/{}/tiles/{}x{}x{}.png'.format(mapname, l, a, b), options='optimize')
+        # The extraction part
+        _extract = 'gdalwarp -of PNG -te {0} {1} {2} {3} -tr {4} {4} -r {5} -multi -wm 2048 {6} temp/out{7}.tif'.format(
+            self.xmin, self.ymin, self.xmax, self.ymax, _scale / 10.0, _sample, _source, _scale)
+        os.system(_extract)
 
-    def render(self):
+        # The rendering part
+        _raster = Image.open('temp/out{}.png'.format(_scale))
+        _wid, _hei = _raster.size
+        _width = (_wid // 256) * 256
+        _height = (_hei // 256) * 256
+        _w = _wid % 256
+        _h = _hei % 256
 
+        _a = _b = _x = _y = 0
 
-    def extract
+        for _y in range(0, _height, 256):
+            for _x in range(0, _width, 256):
+                self._tile(_raster, _level, _x, _y, _a, _b, 256, 256)
+                _b += 1
+            self._tile(_raster, _level, _x + 256, _y, _a, _b, _w, 256)
+            _b = 0
+            _a += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def tiler(src, j, i, l, a, b, xend, yend):
-
-
-
-def level_renderer(level, scale, resampling, source):
-    """
-    :param level: pdfmaps level
-    :param scale: real scale
-    :param resampling: pixel resampling methode
-    :param source: current dataset source
-    :return:
-    """
-    print('\nExtracting level {0} @ 1:{1}.000 scale...'.format(level, scale))
-    extraction = 'gdalwarp -q -of GTiff -te {0} {1} {2} {3} -tr {4} {4} -r {5} -multi -wm 2048 {6} temp/out{7}.tif'.format(
-        xmin, ymin, xmax, ymax, scale / 10.0, resampling, source, scale)
-    os.system(extraction)
-    print('OK')
-
-    print('\nConversion au format PNG...')
-    conversion = 'gdal_translate -q -of PNG -co ZLEVEL=1 temp/out{0}.tif temp/out{0}.png'.format(scale)
-    os.system(conversion)
-    print('OK')
-
-    print('\nSuppression des fichiers annexes...')
-    os.remove('temp/out{}.tif'.format(scale))
-    os.remove('temp/out{}.png.aux.xml'.format(scale))
-    print('OK')
-
-    print('\nCréation des tuiles...')
-    raster = Image.open('temp/out{}.png'.format(scale))
-
-    WIDTH, HEIGHT = raster.size
-    width = (WIDTH // 256) * 256
-    height = (HEIGHT // 256) * 256
-    W = WIDTH % 256
-    H = HEIGHT % 256
-
-    a = b = 0
-
-    for y in range(0, height, 256):
-        for x in range(0, width, 256):
-            tiler(raster, x, y, level, a, b, 256, 256)
-            b += 1
-        tiler(raster, x + 256, y, level, a, b, W, 256)
-        b = 0
-        a += 1
-
-    for x in range(0, width, 256):
-        tiler(raster, x, y + 256, level, a, b, 256, H)
-        b += 1
-    tiler(raster, x + 256, y + 256, level, a, b, W, H)
-    print('OK')
-
-
-def georeferencer():
-    print('\nEcriture du fichier de géoréférencement...')
-    proj = 'PROJCS["RGF93 / Lambert-93",GEOGCS["RGF93",DATUM["Reseau_Geodesique_Francais_1993",SPHEROID["GRS 1980",6378137,298.2572221010002,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6171"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433],AUTHORITY["EPSG","4171"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",49],PARAMETER["standard_parallel_2",44],PARAMETER["latitude_of_origin",46.5],PARAMETER["central_meridian",3],PARAMETER["false_easting",700000],PARAMETER["false_northing",6600000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","2154"]]'
-    width = (xmax - xmin) / 5
-    height = (ymax - ymin) / 5
-    xOrigin = xmin + 2.5
-    yOrigin = ymax - 2.5
-
-    content = '{0}\n{1},5.0,0,{2},0,-5.0\n{3},{4}'.format(proj, xOrigin, yOrigin, width, height)
-
-    ref = open('temp/{0}/{0}.tif.ref'.format(mapname), 'wb')  # wb force strict \n
-    ref.write(bytes(content, 'UTF-8'))
-    ref.close()
-    print('OK')
-
-
-def thumbler():
-    print('\nCréation de la miniature...')
-    os.chdir('/home/elementary/Documents/PDFMaps/temp/{}'.format(mapname))
-
-    img = Image.open('tiles/2x2x2.png')
-    extent = (0, 0, 64, 64)
-    thumb = img.crop(extent)
-    thumb.save('thumb.png')
-    print('OK')
-
-
-def packager():
-    print('\nCompression...')
-    os.chdir('/home/elementary/Documents/PDFMaps/temp/{}'.format(mapname))
-    archive_name = '../{}.zip'.format(mapname)
-
-    archive = zipfile.ZipFile(archive_name, mode="w", allowZip64=True)
-
-    for dirname, subdirs, files in os.walk("."):
-        for filename in files:
-            archive.write(os.path.join(dirname, filename), os.path.join(dirname, filename), zipfile.ZIP_DEFLATED)
-
-    archive.close()
-    os.chdir('/home/elementary/Documents/PDFMaps/')
-    print('OK')
-
-
-def cleaner():
-    print('\nNettoyage')
-    os.system('rm out*.png')
-    os.system('rm -R temp/{0}'.format(mapname))
-    print('OK')
-
+        for _x in range(0, _width, 256):
+            self._tile(_raster, _level, _x, _y + 256, _a, _b, 256, _h)
+            _b += 1
+        self._tile(_raster, _level, _ + 256, _y + 256, _a, _b, _w, _h)
 
 
 if __name__ == '__main__':
 
     # User defined variables
-    mapname = 'toulouse'
-    bbox = (700000, 6500000, 750000, 6550000)
+    mapname = 'test'
+    bounds = (700000, 6500000, 750000, 6550000)
 
     # Advanced config
+    path = os.path.dirname(os.path.realpath(__file__))
+    volume = '/media/DATA'
+    sc25 = '{0}/SIG/IGN/2014/sc25.vrt'.format(volume)
+    sc100 = "{0}/SIG/IGN/2015/sc100.vrt".format(volume)
     levels = 3  # 2 or 3
 
     # Begin of the process
-    pdfmap = Builder(bbox, mapname)
-    pdfmap.extract()
-    pdfmap.render()
-    pdfmap.zip()
-    pdfmap.send()
-    pdfmap.clean()
-
-
-
-
-
-
-
-    # General configuration
-    path = os.path.dirname(os.path.realpath(__file__))
-    volume = "/media/elementary/DATB"
-    vrt_25 = "{0}/IGN/ORIGINAUX/2014/SC25_TOUR_TIF_LZW_LAMB93/fr_25k.vrt".format(volume)
-    vrt_100 = "{0}/IGN/ORIGINAUX/2015/SCAN_100/PNG/fr_100k.vrt".format(volume)
-
-    # Run specific configuration
-    mapname = 'forcalquier'
-    xmin = 900000
-    ymin = 6300000
-    xmax = 950000
-    ymax = 6350000
-
     try:
-        os.mkdir('temp/{0}'.format(mapname))
-        os.mkdir('temp/{0}/tiles'.format(mapname))
 
-        level_renderer(2, 25, 'near', vrt_25)
-        level_renderer(1, 50, 'lanczos', vrt_25)
-        level_renderer(0, 100, 'lanczos', vrt_100)
-
-        georeferencer()
-        thumbler()
-        packager()
-        # cleaner()
+        pdfmap = Builder(bounds, mapname)
+        pdfmap.run(sc25, 2, 25, 'near')
+        pdfmap.run(sc25, 1, 25, 'lanczos')
+        pdfmap.run(sc100, 0, 100, 'lanczos')
 
     except KeyboardInterrupt:
         sys.exit(1)
-
-    print('\nC\'est fini !\n\n\n')

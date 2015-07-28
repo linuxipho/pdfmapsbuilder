cfa29da3e8d543e61653b1404c5d27ac470fed99
linuxipho <remidechazelles@gmail.com>
2015-07-28 10:42:54 +0200

    First attempt

--- main_optimized.py
+++ main_optimized.py
@@ -1,4 +1,13 @@
 __author__ = 'remi'
 
 from osgeo import gdal
+from gdalconst import *
 
+print(gdal.__version__)
+
+in_dataset = gdal.Open("/media/remi/Data/IGN/scan25.vrt", GA_ReadOnly)
+driver = gdal.GetDriverByName("PNG")
+out_dataset = driver.CreateCopy("out.png", in_dataset, 0, ['projwin 600000 6500000 650000 6450000'])
+
+in_dataset = None
+out_dataset = None
